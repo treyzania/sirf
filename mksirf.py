ea88f8167a7285a6ea0d3cb45106d02c3c3aab18
treyzania <treyzania@gmail.com>
2017-06-15 22:27:49 -0400

    More cleanup, made mksirf more verbose.

--- mksirf.py
+++ mksirf.py
@@ -8,9 +8,11 @@ import struct
 path = '.'
 dest = paths.basename(paths.abspath('.')) + '.sird'
 
+verbose = True
+
 if len(sys.argv) > 1:
     path = sys.argv[1]
-    dest = paths.basename(paths.abspath(path)) + '.sird'
+    dest = paths.expanduser(paths.basename(paths.abspath(path)))  + '.sird'
 
 # Now we start to write the files.
 with open(dest, 'wb') as output:
@@ -22,15 +24,28 @@ with open(dest, 'wb') as output:
     records = []
     for a, b, filenames in os.walk(path):
         for f in filenames:
-            records.append(paths.normpath(paths.join(paths.relpath(a, start=path), f))) # Ugly, fix this eventually.s
+            records.append(paths.normpath(paths.join(paths.relpath(a, start=path), f))) # Ugly, fix this eventually.
     output.write(struct.pack('>I', len(records)))
 
+    if verbose:
+        print 'wrote headers'
+
     # Actually pack the files.
-    for r in records:
-        rpath = paths.join(path, r)
-        size = paths.getsize(rpath)
-        header = struct.pack('>QH', size, len(r))
-        output.write(header)
-        output.write(unicode(r, 'utf-8'))
-        with open(rpath, 'rb') as rec:
-            output.write(rec.read())
+    written = 0
+    try:
+        for r in records:
+            rpath = paths.join(path, r)
+            size = paths.getsize(rpath)
+            header = struct.pack('>QH', size, len(r))
+            output.write(header)
+            output.write(unicode(r, 'utf-8'))
+            with open(rpath, 'rb') as rec:
+                if verbose:
+                    print r
+                output.write(rec.read())
+                written += 1
+    except Exception:
+        print 'error:', sys.exc_info()[0]
+
+    if verbose:
+        print 'wrote', str(written), 'records'
