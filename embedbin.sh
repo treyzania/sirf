abaffdc57980a5b719101c6b77f7aae0d616b04c
treyzania <treyzania@gmail.com>
2017-06-17 00:01:43 -0400

    Added more exported symbols from embedbin.sh.

--- embedbin.sh
+++ embedbin.sh
@@ -9,12 +9,19 @@ fi
 
 dest="$name.o"
 
+end_lbl=$name'_end'
+size_lbl=$name'_len'
+size=$(ls $file | awk '{ print $5; }')
+
 echo -e "\
 \t.section .data\n\
 \t.globl $name\n\
-\t.globl fin_$name\n\
+\t.globl $end_lbl\n\
+\t.globl $size_lbl\n\
 $name:\n\
 \t.incbin \"$file\"\n\
-fin_$name:\n\
-\t.byte 0" \
+$end_lbl:\n\
+\t.byte 0\n\
+$size_lbl:\n\
+\t.long $size\n" \
 | gcc -c -x assembler -o $dest -
