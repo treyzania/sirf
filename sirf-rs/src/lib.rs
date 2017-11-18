6dc3c978aa3deac76a4e868789700463283dd3ce
Milo Turner <iitalics@gmail.com>
2017-11-18 02:01:57 -0500

    basic data

--- sirf-rs/src/lib.rs
+++ sirf-rs/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
+/// A SIRD record, based from a byte slice.
+pub struct SIRD<'a> {
+    count: u32,
+    data: &'a [u8],
 }
+
+/// A single record into a SIRD file.
+pub struct Record<'a> {
+    name: &'str,
+    data: &'a [u8],
+}
+
+/// The error type for decoding SIRD files.
+pub enum SIRDError {
+    /// The magic number was not correct.
+    BadMagicNumber,
+
+    /// A record was larger than expected.
+    NotEnoughBytes,
+
+    /// A record name contained invalid UTF-8.
+    StringDecodeError,
+}
+
+/// The magic number expected to appear at the
+/// start of all SIRD files.
+pub const MAGIC: u32 = 0x53495244;
